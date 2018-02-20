b9702263d2f9de56daf8b347c633a0aba4a34f27
Unknown <souhail.merroun@hotmail.com>
2018-02-20 09:50:22 +0000

    Configure firebase with the Console

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 // Firebase app configuration.
 const config = {
     // Past your configuration here from Google Firebase
-    apiKey: "your-apik-ey",
-    authDomain: "your-auth-domain",
-    databaseURL: "your-database-url",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaing-sender-id"
+    apiKey: "AIzaSyBOiPoQn9nmQUMEwkvoeSEUJ-ooB1S9QZE",
+    authDomain: "gdgtangier-4e581.firebaseapp.com",
+    databaseURL: "https://gdgtangier-4e581.firebaseio.com",
+    projectId: "gdgtangier-4e581",
+    storageBucket: "gdgtangier-4e581.appspot.com",
+    messagingSenderId: "321517419944"
 }
 
 export default config;
