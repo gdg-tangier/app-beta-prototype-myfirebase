daa4c346b9b24977bc941c685a10388ecc55baee
Amrani Houssain <amranidev@gmail.com>
2018-02-21 22:39:30 +0000

    update ProfileUpdate.vue

--- src/components/ProfileUpdate.vue
+++ src/components/ProfileUpdate.vue
@@ -1,12 +1,11 @@
 <template>
-    
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <h1>Update Profile</h1>
             </div>
         </div>
-        <div class="row"> 
+        <div class="row">
             <div class="col-md-6">
                 <div class="card card-default">
                     <div class="card-header">Preview</div>
@@ -30,31 +29,31 @@
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label text-md-right">Name</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" v-model="name">
+                                    <input type="text" class="form-control" v-model="Profile.name">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label text-md-right">Bio</label>
                                 <div class="col-md-6">
-                                    <textarea class="form-control" v-model="bio" rows="4"></textarea>
+                                    <textarea class="form-control" v-model="Profile.bio" rows="4"></textarea>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label text-md-right">City</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" v-model="city">
+                                    <input type="text" class="form-control" v-model="Profile.city">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label text-md-right">Age</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" v-model="age">
+                                    <input type="text" class="form-control" v-model="Profile.age">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label text-md-right">Phone Number</label>
                                 <div class="col-md-6">
-                                    <input type="text" class="form-control" v-model="phone_number">
+                                    <input type="text" class="form-control" v-model="Profile.phone_number">
                                 </div>
                             </div>
                             <div class="form-group row mb-0">
@@ -70,85 +69,55 @@
             </div>
         </div>
     </div>
-    
 </template>
 
 <script>
+import Profile from "./../models/profile"
 
-    export default {
+export default {
 
-        mounted() {
+    mounted() {
 
-            this.isLoading = true
+        this.isLoading = true
 
-            this.$auth.check({
-                then: (user) => {
-                    this.user = user
-
-                    let ref = this.$store.state.firebase.database().ref('users')
-
-                    let self = this;
-
-                    ref.once('value', function(snapshot) {
-                        if (snapshot.hasChild(user.uid)) {
-                            ref.on('value', (data) => {
-
-                                let item = data.val()[user.uid]
-
-                                self.name = item.name
-                                self.bio = item.bio
-                                self.city = item.city
-                                self.age = item.age
-                                self.phone_number = item.phone_number
-
-                            }, (error) => {
-                                console.log("I could not get user data")
-                            })
+        this.$auth.check({
+            then: (user) => {
+                this.$bindAsObject("loggedIn",this.$store.state.firebase.database().ref(`users/${user.uid}`),null,
+                    () => {
+                        if (this.loggedIn) {
+                            this.Profile.setName(this.loggedIn.name)
+                                .setCity(this.loggedIn.city)
+                                .setBio(this.loggedIn.bio)
+                                .setPhoneNumber(this.loggedIn.phone_number)
+                                .setAge(this.loggedIn.age)
                         }
-                        else {
-                            console.log("First time logged In")
-                        }
-
-                        self.isLoading = false
-                    });
-                },
-                catch: () => { 
-                    this.isLoading = false
-                    console.log("I could not get the authentificated user")
-                }
-            });
-
-        },
-        data() {
-            return {
-                isLoading: false,
-
-                user: '',
-
-                name: '',
-                bio: '',
-                city: '',
-                age: '',
-                phone_number: ''
+                        this.isLoading = false
+                    })
+            },
+            catch: () => {
+                this.isLoading = false
+                console.log("I could not get the authentificated user")
             }
-        },
-        methods: {
-            update() {
-                this.isLoading = true
+        });
 
-                let ref = this.$store.state.firebase.database().ref('users')
-
-                ref.child(this.user.uid).set({
-                    name: this.name,
-                    bio: this.bio,
-                    city: this.city,
-                    age: this.age,
-                    phone_number: this.phone_number
-                }, () => {
+    },
+    data() {
+        return {
+            isLoading: false,
+            Profile: new Profile(),
+        }
+    },
+    methods: {
+        update() {
+            this.$firebaseRefs.loggedIn
+                .set(this.Profile.toJson()).then(() => {
                     this.isLoading = false
-                });
-            }
+                })
+                .catch((err) => {
+                    console.error(err.message)
+                })
         }
     }
+}
 
 </script>
